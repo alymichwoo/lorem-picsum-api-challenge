b065b235e65c5d896727b8f7607cb8ff007b9555
Alyssa Woods <alyssawoods3@gmail.com>
2023-08-28 18:47:40 -0700

    Select and display random image and name of author

--- js/script.js
+++ js/script.js
@@ -9,6 +9,25 @@ const getImage = async function () {
     const res = await fetch ("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+//Selects a single random image object
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+};
+
+//Displays the image and author
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+    getImage();
+});
